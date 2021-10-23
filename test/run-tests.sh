103614f5cfe88ff199f3b43ecf6c3a96cfc81b8c
Adam Trojanowski <adam@inspur.localnet>
2021-10-23 17:36:09 -0400

    Removed test needing a terminal

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -3,15 +3,15 @@
 set -e -x
 
 
-./build/test/test-+
+#./build/test/test-+
 ./build/test/test-histogram
 #./build/test/test-line-spin
-./build/test/test-rnd10000
-./build/test/test-rnd10000-bar
-./build/test/test-rnd10000-line
+#./build/test/test-rnd10000
+#./build/test/test-rnd10000-bar
+#./build/test/test-rnd10000-line
 ./build/test/test-simple
 ./build/test/test-trig
 #./build/test/test-trig-loop
-./build/test/test-x
+#./build/test/test-x
 
 echo "all tests completed"
